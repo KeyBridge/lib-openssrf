0cf24664d287497faf9a37e17bdba33631c9a397
Jesse Caulfield <jesse@caulfield.org>
2015-03-31 08:44:11 -0400

    manually correct reference fields

--- src/main/java/us/gov/dod/standard/ssrf/_3_1/ForceElement.java
+++ src/main/java/us/gov/dod/standard/ssrf/_3_1/ForceElement.java
@@ -40,7 +40,7 @@ import us.gov.dod.standard.ssrf._3_1.metadata.lists.*;
   "type",
   "uic",
   "owningCountry",
-  "owningOrganisation",
+  "owningOrganisationRef",
   "role",
   "platform",
   "missionCode",
