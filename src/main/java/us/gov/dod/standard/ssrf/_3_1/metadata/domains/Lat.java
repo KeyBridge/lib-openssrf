958b40eddaee52ed7aff2ec34176c2c70e0deab1
Jesse Caulfield <jesse@caulfield.org>
2015-04-04 12:38:05 -0400

    rewrite to use Decimal degrees as the internal storage value

--- src/main/java/us/gov/dod/standard/ssrf/_3_1/metadata/domains/Lat.java
+++ src/main/java/us/gov/dod/standard/ssrf/_3_1/metadata/domains/Lat.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2015 Key Bridge LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,31 +15,45 @@
  */
 package us.gov.dod.standard.ssrf._3_1.metadata.domains;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.XmlValue;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import us.gov.dod.standard.ssrf._3_1.adapter.types.XmlAdapterLAT;
+import us.gov.dod.standard.ssrf._3_1.adapter.XmlAdapterLAT;
 import us.gov.dod.standard.ssrf._3_1.metadata.AMetadata;
 
 /**
- * The SSRF Lat data type.
+ * The SSRF Latitude data type.
+ * <p>
+ * The geographical latitude (degrees, minutes, seconds and hemisphere N or S)
+ * of the Point or center of the Ellipse. Same remarks for seconds and leading
+ * zeros. enter N or S immediately following the seconds. The format is:
+ * <code>ddmmss.hhH</code> (where ".hh" is optional and H = N or S).
+ * <p>
+ * [XSD ERR REGEX] This data item MUST comply to the regular expression:
+ * "(((((([0-8][0-9]))([0-5][0-9]){2})(. [0-9]{1,2})?)|900000)(N|S))|X"
+ * <p>
+ * Developer note: This <code>Lat</code> is an extension and replacement
+ * implementation of the default <code>Lat</code> using a BigDecimal value in
+ * Decimal degrees (instead of DMS-encoded string) and relying upon the
+ * XmlJavaTypeAdapter for DMS translation.
  * <p>
  * @author Key Bridge LLC <developer@keybridge.ch>
- * @version 3.1.0, 03/30/2015
+ * @version 3.1.0, 04/04/2015
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "Lat", propOrder = {"value"})
 public class Lat extends AMetadata<Lat> implements Comparable<Lat> {
 
   /**
-   * The value property.
+   * The value property in decimal degrees.
    */
   @XmlValue
   @XmlJavaTypeAdapter(XmlAdapterLAT.class)
-  protected String value;
+  protected BigDecimal value;
 
   /**
    * Construct a new, empty Lat instance.
@@ -50,27 +64,54 @@ public class Lat extends AMetadata<Lat> implements Comparable<Lat> {
   /**
    * Construct a new Lat instance with the given initial value.
    * <p>
-   * @param value The initial value.
+   * @param value The initial value in decimal degrees.
    */
-  public Lat(String value) {
+  public Lat(BigDecimal value) {
     this.value = value;
+  }
+
+  /**
+   * Construct a new Lat instance with the given initial value.
+   * <p>
+   * @param value The initial value in decimal degrees.
+   */
+  public Lat(Double value) {
+    this.value = BigDecimal.valueOf(value);
+  }
+
+  /**
+   * Construct a new Lat instance with the given initial value.
+   * <p>
+   * Sets the geographical latitude (degrees, minutes, seconds and hemisphere N
+   * or S) of the source of the interference. Same remarks for seconds and
+   * leading zeros. Enter N or S immediately following the seconds. The format
+   * is: ddmmss.hhH (where ".hh" is optional and H = N or S).
+   * <p>
+   * [XSD ERR REGEX] This data item MUST comply to the regular expression:
+   * "(((((([0-8][0-9]))([0-5][0-9]){2})(.[0-9]{1,2})?)|900000)(N|S))|X"
+   * <p>
+   * @param value The initial value as aDMS encoded string
+   * @throws Exception if the value fails to parse
+   */
+  public Lat(String value) throws Exception {
+    this.value = (BigDecimal) new XmlAdapterLAT().unmarshal(value);
   }
 
   /**
    * Get the value of the value property.
    * <p>
-   * @return the value of the value property
+   * @return the value of the value property in decimal degrees.
    */
-  public String getValue() {
+  public BigDecimal getValue() {
     return value;
   }
 
   /**
    * Set the value of the value property.
    * <p>
-   * @param the value
+   * @param the value in decimal degrees.
    */
-  public void setValue(String value) {
+  public void setValue(BigDecimal value) {
     this.value = value;
   }
 
@@ -103,7 +144,7 @@ public class Lat extends AMetadata<Lat> implements Comparable<Lat> {
    */
   @Override
   public String toString() {
-    return this.value;
+    return this.value != null ? this.value.toString() : null;
   }
 
   //<editor-fold defaultstate="collapsed" desc="Hashcode Equals and Comparable">
--- src/main/java/us/gov/dod/standard/ssrf/_3_1/metadata/domains/Lon.java
+++ src/main/java/us/gov/dod/standard/ssrf/_3_1/metadata/domains/Lon.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2015 Key Bridge LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,31 +15,50 @@
  */
 package us.gov.dod.standard.ssrf._3_1.metadata.domains;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.XmlValue;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import us.gov.dod.standard.ssrf._3_1.adapter.types.XmlAdapterLON;
+import us.gov.dod.standard.ssrf._3_1.adapter.XmlAdapterLON;
 import us.gov.dod.standard.ssrf._3_1.metadata.AMetadata;
 
 /**
- * The SSRF Lon data type.
+ * The SSRF Longitude data type.
+ * <p>
+ * The geographical longitude (degrees, minutes, seconds, and hemisphere E or W)
+ * of the Point or center of the Ellipse. If the seconds are not known, use 00,
+ * except in the case of navigation aid systems, geostationary satellites, and
+ * microwave facilities where seconds are required. Use leading zeros as
+ * appropriate for degrees, minutes, or seconds. Degrees longitude require three
+ * digits. Seconds may have a decimal point followed by up to two decimals.
+ * enter E or W immediately following the seconds. The format is: dddmmss.hhH
+ * (where ".hh" is optional and H = E or W).
+ * <p>
+ * [XSD ERR REGEX] This data item MUST comply to the regular expression:
+ * "((((((0[0-9]{2})|(1[0-7][0-9]))
+ * ([0-5][0-9]){2})(.[0-9]{1,2})?)|1800000)(E|W))|X"
+ * <p>
+ * Developer note: This <code>Lon</code> is an extension and replacement
+ * implementation of the default <code>Lon</code> using a BigDecimal value in
+ * Decimal degrees (instead of DMS-encoded string) and relying upon the
+ * XmlJavaTypeAdapter for DMS translation.
  * <p>
  * @author Key Bridge LLC <developer@keybridge.ch>
- * @version 3.1.0, 03/30/2015
+ * @version 3.1.0, 04/04/2015
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "Lon", propOrder = {"value"})
 public class Lon extends AMetadata<Lon> implements Comparable<Lon> {
 
   /**
-   * The value property.
+   * The value property in decimal degrees.
    */
   @XmlValue
   @XmlJavaTypeAdapter(XmlAdapterLON.class)
-  protected String value;
+  protected BigDecimal value;
 
   /**
    * Construct a new, empty Lon instance.
@@ -50,27 +69,58 @@ public class Lon extends AMetadata<Lon> implements Comparable<Lon> {
   /**
    * Construct a new Lon instance with the given initial value.
    * <p>
-   * @param value The initial value.
+   * @param value The initial value in decimal degrees.
    */
-  public Lon(String value) {
+  public Lon(BigDecimal value) {
     this.value = value;
+  }
+
+  /**
+   * Construct a new Lon instance with the given initial value.
+   * <p>
+   * @param value The initial value in decimal degrees.
+   */
+  public Lon(Double value) {
+    this.value = BigDecimal.valueOf(value);
+  }
+
+  /**
+   * Construct a new Lon instance with the given initial value.
+   * <p>
+   * Sets the geographical longitude (degrees, minutes, seconds, and hemisphere
+   * E or W) of the source of the interference. If the seconds are not known,
+   * use 00. Use leading zeros as appropriate for degrees, minutes, or seconds.
+   * Degrees longitude require three digits. Seconds may have a decimal point
+   * followed by up to two decimals. Enter E or W immediately following the
+   * seconds. The format is: dddmmss.hhH (where ".hh" is optional and H = E or
+   * W).
+   * <p>
+   * [XSD ERR REGEX] This data item MUST comply to the regular expression:
+   * "((((((0[0-9]{2})|(1[0-7][0-9]))([0-5]
+   * [0-9]){2})(.[0-9]{1,2})?)|1800000)(E|W))|X"
+   * <p>
+   * @param value The initial value as a string encoded DMS value.
+   * @throws Exception if the value fails to parse
+   */
+  public Lon(String value) throws Exception {
+    this.value = (BigDecimal) new XmlAdapterLON().unmarshal(value);
   }
 
   /**
    * Get the value of the value property.
    * <p>
-   * @return the value of the value property
+   * @return the value of the value property in decimal degrees.
    */
-  public String getValue() {
+  public BigDecimal getValue() {
     return value;
   }
 
   /**
    * Set the value of the value property.
    * <p>
-   * @param the value
+   * @param the value in decimal degrees.
    */
-  public void setValue(String value) {
+  public void setValue(BigDecimal value) {
     this.value = value;
   }
 
@@ -103,7 +153,7 @@ public class Lon extends AMetadata<Lon> implements Comparable<Lon> {
    */
   @Override
   public String toString() {
-    return this.value;
+    return this.value != null ? this.value.toString() : null;
   }
 
   //<editor-fold defaultstate="collapsed" desc="Hashcode Equals and Comparable">
