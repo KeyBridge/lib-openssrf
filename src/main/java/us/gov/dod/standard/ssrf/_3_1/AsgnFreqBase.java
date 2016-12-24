57e064967627b9e36616691ee09dcac0748ccd9f
Brad Hards <bradh@frogmouth.net>
2016-12-24 12:22:41 +1100

    [javadoc] Escape email in AsgnFreqBase docs.

--- src/main/java/us/gov/dod/standard/ssrf/_3_1/AsgnFreqBase.java
+++ src/main/java/us/gov/dod/standard/ssrf/_3_1/AsgnFreqBase.java
@@ -25,7 +25,7 @@ import us.gov.dod.standard.ssrf._3_1.metadata.lists.ListCAU;
 /**
  * The SSRF AsgnFreqBase data type.
  *
- * @author Key Bridge LLC <developer@keybridge.ch>
+ * @author Key Bridge LLC &lt;developer@keybridge.ch&gt;
  * @version 3.1.0, 03/30/2015
  */
 @XmlAccessorType(XmlAccessType.FIELD)
